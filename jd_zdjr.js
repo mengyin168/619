4f58c4342649fd130d043555b3f6f011ea5310c8
mengyin168 <87105031+mengyin168@users.noreply.github.com>
2022-04-05 19:45:22 +0800

    Add files via upload

--- jd_zdjr.js
+++ jd_zdjr.js
@@ -1,15 +1,10 @@
 /*
-
 一共有2个变量
 jd_zdjr_activityId  活动ID 必需
 jd_zdjr_activityUrl 活动地址 必需
-
 已适配docker
-
 需要配合重写获取=>活动id、活动地址
-
 https://\w+-isv.isvjcloud.com/wxTeam/shopInfo url script-request-body jd_zdjr.js
-
 mitm
 *-isv.isvjcloud.com
 [task_local]
@@ -18,7 +13,6 @@ mitm
 ================Loon==============
 [Script]
 cron "40 11 * * *" script-path=jd_zdjr.js,tag=组队瓜分京豆
-
 */
 
 let jd_zdjr_activityId = ''// 活动ID
